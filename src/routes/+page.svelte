e44fd01b0a5bf02bb23fe0c9dfe14283f1338163
Matheus Souza <88462129+mtzsouza@users.noreply.github.com>
2023-09-08 13:49:29 -0500

    quick fix on png not loading after deployment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,8 @@
 </script>
 
 <div class="container max-w-[80%] sm:max-w-[30%] flex flex-col justify-center items-center">
-    <h1 class="py-10 antialiased text-4xl font-madi text-gray-900 font-semibold tracking-widest select-none">
+    <h1 class="py-8 antialiased text-4xl font-madi text-gray-900 font-semibold tracking-widest select-none">
         Coming soon.
     </h1>
-    <!-- svelte-ignore a11y-img-redundant-alt -->
-    <img src="src\lib\images\image.png" alt="image" class="animate-bounce">
+    <img src="https://i.imgur.com/rCAUHwF.png" alt="bg" class="animate-bounce">
 </div>
