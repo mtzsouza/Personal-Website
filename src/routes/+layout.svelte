93d10289a2624521c7df4ab22843a8f2ed5fc9cf
Matheus Souza <88462129+mtzsouza@users.noreply.github.com>
2023-09-15 12:11:56 -0500

    added new routes and configured links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,51 +1,56 @@
 <script lang='ts'>
-    import "../app.css";
+  import "../app.css";
 
-    let menuOpen = false;
-    let menuItems = false;
+  export let data;
 
-    const toggleMenu = () => {
-      menuOpen = !menuOpen;
-      if (menuOpen) {
-        setTimeout(function(){
-          menuItems = !menuItems;
-        }, 180)
-      } else {
-        setTimeout(function(){
-          menuItems = !menuItems;
-        }, 100)
-      }
-    };
+  $: currentPage = data.url;
+
+  let menuOpen = false;
+  let menuItems = false;
+
+  const toggleMenu = () => {
+    menuOpen = !menuOpen;
+    if (menuOpen) {
+      setTimeout(function(){
+        menuItems = !menuItems;
+      }, 200)
+    } else {
+      setTimeout(function(){
+        menuItems = !menuItems;
+      }, 100)
+    }
+  };
 
 </script>
   
 <div class="h-screen flex flex-col items-center 
             bg-gradient-to-br from-gray-900 via-gray-950 to-gray-950">
   <nav class="h-10 w-[85%] bg-blue-950 bg-opacity-30 z-20 my-4 px-8 sm:px-16 rounded-lg sm:h-12 sm:w-[92%]
-          flex flex-col antialiased text-gray-100 transition-all duration-500"
-          class:h-20={menuOpen}>
+          flex flex-col antialiased text-gray-100 transition-all duration-500" class:h-20={menuOpen}>
           <div class="w-full flex flex-row justify-between items-center antialiased">
-            <h1 class="font-madi text-4xl sm:text-6xl cursor-pointer relative sm:right-6 sm:top-1">
+            <a href="/" class="font-madi text-4xl sm:text-6xl cursor-pointer relative sm:right-6 sm:top-1">
               Matheus Souza
-            </h1>
+            </a>
             <!-- svelte-ignore a11y-click-events-have-key-events -->
             <!-- svelte-ignore a11y-no-static-element-interactions -->
-            <div class="sm:hidden text-xl opacity-70" on:click={toggleMenu}>
+            <div class="sm:hidden text-xl opacity-80" on:click={toggleMenu}>
               <i class="fa-solid fa-bars" style="color: #ffffff;"></i>
             </div>
+            <!-- Desktop menu items -->
             <div class="hidden sm:flex relative bottom-1.5 gap-24">
-              <div class="navItem active">Home</div>
-              <div class="navItem">Projects</div>
-              <div class="navItem">Contact</div>
+              <a href="/" class="navItem" class:active={currentPage == '/'}>Home</a>
+              <a href="/projects" class="navItem" class:active={currentPage == '/projects'}>Projects</a>
+              <a href="/contact" class="navItem" class:active={currentPage == '/contact'}>Contact</a>
             </div>
           </div>
+          <!-- Mobile menu items -->
           <div class="sm:hidden py-3 w-full flex justify-around" class:hidden={!menuItems}>
-            <div class="navItem active">Home</div>
-            <div class="navItem">Projects</div>
-            <div class="navItem">Contact</div>
+            <a href="/" class="navItem" class:active={currentPage == '/'}>Home</a>
+            <a href="/projects" class="navItem" class:active={currentPage == '/projects'}>Projects</a>
+            <a href="/contact" class="navItem" class:active={currentPage == '/contact'}>Contact</a>
           </div>
   </nav>
   <div class="h-[85%] w-[85%] sm:w-[92%] rounded-lg flex items-center justify-center bg-blue-950 bg-opacity-30">
-    <slot />
+    <slot/>
   </div>
 </div>
