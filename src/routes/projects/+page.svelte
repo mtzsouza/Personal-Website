0f2b12907bd1e3436333cd61e51115c433f36ac0
Matheus Souza <88462129+mtzsouza@users.noreply.github.com>
2023-09-28 01:20:00 -0500

    styling changes

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -26,35 +26,35 @@
     <div class="h-full w-full grid grid-flow-col items-center gap-20">
         <a href="https://www.youtube.com/watch?v=BWpijxhNzvY" target="_blank" rel="noreferrer noopener">
             <div class="rounded-lg border-solid border-blue-950 border-2 overflow-hidden 
-            hover:scale-110 transform transition duration-300">
+            hover:scale-110 hover:skew-y-2 hover:-skew-x-2 transform transition duration-300">
                 <img src="https://i.imgur.com/5qiqfsX.png" alt="">
             </div>
         </a>
         <a href="" class="cursor-default rounded-lg border-solid overflow-hidden">
-            <div class="rounded-lg border-solid border-blue-950 border-2 overflow-hidden">
-                <img src="https://i.imgur.com/qLCcJLy.png" alt="" class="opacity-10">
+            <div class="rounded-lg border-solid border-blue-950 border-2 overflow-hidden opacity-10">
+                <img src="https://i.imgur.com/qLCcJLy.png" alt="">
             </div>
         </a>
         <a href="" class="cursor-default rounded-lg border-solid overflow-hidden">
-            <div class="rounded-lg border-solid border-blue-950 border-2 overflow-hidden">
-                <img src="https://i.imgur.com/qLCcJLy.png" alt="" class="opacity-10">
+            <div class="rounded-lg border-solid border-blue-950 border-2 overflow-hidden opacity-10">
+                <img src="https://i.imgur.com/qLCcJLy.png" alt="">
             </div>
         </a>
     </div>
     <div class="h-full w-full grid grid-flow-col items-center gap-20">
         <a href="" class="cursor-default rounded-lg border-solid overflow-hidden">
-            <div class="rounded-lg border-solid border-blue-950 border-2 overflow-hidden">
-                <img src="https://i.imgur.com/qLCcJLy.png" alt="" class="opacity-10">
+            <div class="rounded-lg border-solid border-blue-950 border-2 overflow-hidden opacity-10">
+                <img src="https://i.imgur.com/qLCcJLy.png" alt="">
             </div>
         </a>
         <a href="" class="cursor-default rounded-lg border-solid overflow-hidden">
-            <div class="rounded-lg border-solid border-blue-950 border-2 overflow-hidden">
-                <img src="https://i.imgur.com/qLCcJLy.png" alt="" class="opacity-10">
+            <div class="rounded-lg border-solid border-blue-950 border-2 overflow-hidden opacity-10">
+                <img src="https://i.imgur.com/qLCcJLy.png" alt="">
             </div>
         </a>
         <a href="" class="cursor-default rounded-lg border-solid overflow-hidden">
-            <div class="rounded-lg border-solid border-blue-950 border-2 overflow-hidden">
-                <img src="https://i.imgur.com/qLCcJLy.png" alt="" class="opacity-10">
+            <div class="rounded-lg border-solid border-blue-950 border-2 overflow-hidden opacity-10">
+                <img src="https://i.imgur.com/qLCcJLy.png" alt="">
             </div>
         </a>
     </div>
