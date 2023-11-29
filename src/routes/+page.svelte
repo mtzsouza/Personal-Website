a0fb94e044c40cecbe4c07ce9f6839466f820696
Matheus Souza <88462129+mtzsouza@users.noreply.github.com>
2023-11-28 22:24:19 -0600

    styling changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,17 @@
 </script>
 
 <!-- Mobile -->
-<div class="sm:hidden max-h-[95%] max-w-[90%] grid grid-rows-3
+<div class="sm:hidden max-h-[95%] max-w-[90%] grid grid-rows-3 py-4
             text-gray-100 antialiased font-bubbler">
     <div class="flex justify-center relative top-4">
         <img src="https://i.imgur.com/Uo4nGZx.png" alt="img" class="object-cover overflow-visible animate-bounce">
     </div>
-    <div class="flex flex-col gap-12 text-center text-3xl relative top-4">
+    <div class="flex flex-col gap-2 text-2xl relative top-4 px-4">
         <div>
-            <span>I'm <span class="font-semibold">Matheus</span>,</span>
-            <br>
-            <span>undergraduate Computer Science student.</span>
+            <span class="font-semibold">Hello,</span>
+        </div>
+        <div class="pb-8">
+            <span>I'm a forward-thinking CS student, adept in full-stack development and crafting practical solutions with code.</span>
         </div>
         <div class="flex gap-5 justify-center">
             <a href="https://github.com/mtzsouza" target="_blank" rel="noreferrer noopener">
@@ -22,11 +23,8 @@
                 <i class="fa-brands fa-linkedin fa-xl" style="color: #ffffff;"></i>
             </a>
             <a href="https://drive.google.com/file/d/143N5jNXtM11ft7_a-jYSAOHDMIHty0jL/view?usp=sharing" target="_blank" rel="noreferrer noopener">
-                <i class="fa-solid fa-file-pdf fa-xl" style="color: #ffffff;"></i>
+                <i class="fa-solid fa-file-lines fa-xl" style="color: #ffffff;"></i>
             </a>
-        </div>
-        <div class="pb-8">
-            <span>Creative fullstack developer and avid game design enthusiast who seeks new tech interests and opportunities to grow.</span>
         </div>
     </div>
 </div>
@@ -34,13 +32,12 @@
 <!-- Desktop -->
 <div class="hidden sm:flex max-h-[95%] max-w-[90%] grid grid-flow-col grid-cols-2 justify-around overflow-hidden
             text-gray-100 antialiased font-bubbler">
-    <div class="flex flex-col gap-12 max-w-[50%] tracking-widest">
+    <div class="flex flex-col gap-6 max-w-[50%] tracking-widest">
         <div class="text-4xl pt-12">
-            <span>I'm <span class="font-semibold">Matheus</span>,</span>
-            <span>undergraduate Computer Science student.</span>
+            <span class="font-semibold">Hello!</span>
         </div>
         <div class="text-3xl">
-            <span>Creative fullstack developer and avid game design enthusiast who seeks new tech interests and opportunities to grow.</span>
+            <span>I'm a forward-thinking CS student, adept in full-stack development and crafting practical solutions with code.</span>
         </div>
         <div class="flex gap-5 justify-center text-3xl">
             <a href="https://github.com/mtzsouza" target="_blank" rel="noreferrer noopener">
@@ -50,11 +47,11 @@
                 <i class="fa-brands fa-linkedin fa-xl" style="color: #ffffff;"></i>
             </a>
             <a href="https://drive.google.com/file/d/143N5jNXtM11ft7_a-jYSAOHDMIHty0jL/view?usp=sharing" target="_blank" rel="noreferrer noopener">
-                <i class="fa-solid fa-file-pdf fa-xl" style="color: #ffffff;"></i>
+                <i class="fa-solid fa-file-lines fa-xl" style="color: #ffffff;"></i>
             </a>
         </div>
     </div>
-    <div class="flex justify-center max-w-[50%] relative bottom-10">
+    <div class="flex justify-center max-w-[30%] relative bottom-10">
         <img src="https://i.imgur.com/Uo4nGZx.png" alt="img" class="object-cover overflow-visible animate-bounce">
     </div>
 </div>
